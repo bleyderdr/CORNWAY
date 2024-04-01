cad875f28807ea7c339ba5e1f62ffa0655bf5521
bleyderdr <bladimirsl123@gmail.com>
2024-04-01 16:56:44 -0500

    Add project files.

---
+++ CORNWAY/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace CORNWAY.Model;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicatioDbContext : DbContext
+{
+    public DbSet<Personaje> Personajes { get; set; }
+    public DbSet<Enemigo> Enemigos { get; set; }
+    public DbSet<Mascota> Mascotas { get; set; }
+    public DbSet<Arma> Armas { get; set; }
+    public DbSet<Objeto> Objetos { get; set; }
+    public DbSet<Semilla> Semillas { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+}
