f6015a1ca52e9b29f1e48fab1b5d4bb15015dc66
bleyderdr <bladimirsl123@gmail.com>
2024-04-15 21:40:24 -0500

    Repository

---
+++ CORNWAY/Repositories/ArmaRepository.cs
@@ -0,0 +1,45 @@
+﻿using CORNWAY.Context;
+using CORNWAY.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CORNWAY.Repositories
+{
+    public interface IArmaRepository
+    {
+        Task<IEnumerable<Arma>> GetArmas();
+        Task<Arma?> GetArmaById(int id);
+        Task<Arma> CreateArma(Arma arma);
+        Task<Arma> PutArma(Arma arma);
+        Task<Arma?> DeleteArma(int id);
+    }
+
+    public class ArmaRepository(ApplicationDbContext db) : IArmaRepository
+    {
+        public async Task<Arma?> GetArma(int id)
+        {
+            return await db.Arma.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Arma>> GetArmas()
+        {
+            return await db.Arma.ToListAsync();
+        }
+
+        public async Task<Arma> CreateArma(Arma arma)
+        {
+            db.Arma.Add(arma);
+            await db.SaveChangesAsync();
+            return arma;
+        }
+
+        public async Task<Arma> PutArma(Arma arma)
+        {
+            db.Entry(arma).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            return arma;
+        }
+
+       
+
+    }
+}
